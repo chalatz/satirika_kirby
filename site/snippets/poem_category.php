a5121183fef029ff2051ee30b683acc321cbb27f
chalatz <chralatz@gmail.com>
2016-05-13 10:32:32 +0300

    ok

--- site/snippets/poem_category.php
+++ site/snippets/poem_category.php
@@ -2,6 +2,6 @@
     <?php foreach($page->tags()->split() as $tag): ?>
         <a class="poem-category-item" href="<?php echo $page->get_category_url($tag) ?>">
             <?php echo $tag ?>
-        </a>
+        </a>        
     <?php endforeach ?>
 </section>
