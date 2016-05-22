78e9906f9cfc1b54e1ef81e956df0c7dc7c2ded3
chalatz <chralatz@gmail.com>
2016-05-22 17:53:03 +0300

    Corrected a typo

--- site/controllers/poems.php
+++ site/controllers/poems.php
@@ -49,7 +49,7 @@ return function($site, $pages, $page) {
         if($category == 'filosofika') {
             $poems = $page->children()
                 ->sortBy('date', 'desc', 'time', 'desc')
-                ->filterBy('tags', 'Φυλοσοφικά')
+                ->filterBy('tags', 'Φιλοσοφικά')
                 ->paginate($site->pageNum()->int());
 
             $title = 'Φιλοσοφικά';
