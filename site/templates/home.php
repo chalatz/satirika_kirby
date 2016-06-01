357001d084198e807416e8330ae5d600f7046229
chalatz <chralatz@gmail.com>
2016-06-01 12:07:12 +0300

    Added all poems link at home page

--- site/templates/home.php
+++ site/templates/home.php
@@ -8,13 +8,21 @@
         <?php snippet('header') ?>
 
         <section role="main" class="content-container teasers">
+
             <h1 class="title">
                 Τελευταία Ποιήματα
             </h1>
+
             <?php $poems = page('poems')->children()->sortBy('date', 'desc', 'time', 'desc')->limit($site->frontPageNum()->int()) ?>
+
             <?php foreach($poems as $poem): ?>
                 <?php snippet('poem-teaser', ['poem' => $poem]) ?>
             <?php endforeach ?>
+
+            <a href="<?php echo url('poems') ?>" class="all-poems-link">
+                Όλα τα ποιήματα &rarr;
+            </a>
+
         </section>
 
         <?php snippet('footer') ?>
