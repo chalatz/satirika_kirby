6a5c9e5355658d8315f567ec168cb617bca4de21
chalatz <chralatz@gmail.com>
2016-06-01 11:55:33 +0300

    Added search

--- site/templates/search.php
+++ site/templates/search.php
@@ -7,17 +7,23 @@
 
         <?php snippet('header') ?>
 
-        <section role="main" class="content-container">
+        <section role="main" class="content-container teasers">
 
-            <ul>
-                <?php foreach($results as $result): ?>
-                <li>
-                  <a href="<?php echo $result->url() ?>">
-                    <?php echo $result->title()->html() ?>
-                  </a>
-                </li>
+            <h1 class="title">Αποτελέσματα για: <span class="given-search-query"><?php echo $query ?></span></h1>
+
+            <?php if($results->count() == 0): ?>
+
+                <p class="not-found">
+                    Δε βρέθηκαν ποιήματα για την αναζήτησή σας.
+                </p>
+
+            <?php else: ?>
+
+                <?php foreach($results as $poem): ?>
+                    <?php snippet('poem-teaser', ['poem' => $poem]) ?>
                 <?php endforeach ?>
-            </ul>
+
+            <?php endif ?>
 
         </section>
 
