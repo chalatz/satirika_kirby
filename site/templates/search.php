ef1d14bd363b0f6a59db8dc74943971c36f66017
chalatz <chralatz@gmail.com>
2016-06-02 15:14:39 +0300

    Added sitemap

--- site/templates/search.php
+++ site/templates/search.php
@@ -9,15 +9,17 @@
 
         <section role="main" class="content-container teasers">
 
-            <h1 class="title">Αποτελέσματα για: <span class="given-search-query"><?php echo $query ?></span></h1>
-
             <?php if($results->count() == 0): ?>
+
+                <h1 class="title">Αποτελέσματα Αναζήτησης</h1>
 
                 <p class="not-found">
                     Δε βρέθηκαν ποιήματα για την αναζήτησή σας.
                 </p>
 
             <?php else: ?>
+
+                <h1 class="title">Αποτελέσματα για: <span class="given-search-query"><?php echo $query ?></span></h1>
 
                 <?php foreach($results as $poem): ?>
                     <?php snippet('poem-teaser', ['poem' => $poem]) ?>
