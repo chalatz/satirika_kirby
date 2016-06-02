e483bc0bdeb6bfb24ca4b29e5ec370ca6568347f
chalatz <chralatz@gmail.com>
2016-06-02 19:52:24 +0300

    Added site builder

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,5 +1,13 @@
 <footer class="site-footer">
     <div class="footer-content">
-        <p>copyright, <?php echo date("Y") ?></p>
+
+        <p>
+            copyright, <?php echo date("Y") ?>
+        </p>
+
+        <p>
+            Σχεδίαση, ανάπτυξη: <a class="footer-link" href="https://twitter.com/chalatz">Χρήστος Αλατζάς</a>
+        </p>
+
     </div>
 </footer>
