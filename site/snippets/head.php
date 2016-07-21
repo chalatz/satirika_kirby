26bbaa07527703fdbbaa5f7b0f9a8ffa3c12e61b
chalatz <chralatz@gmail.com>
2016-07-21 17:15:12 +0300

    Added apple touch icon

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -11,7 +11,7 @@
     <meta name="description" content="<?php echo $site->description() ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="apple-touch-icon" href="<?php echo url('assets/images/apple-touch-icon.png') ?>">
     <link rel="shortcut icon" href="<?php echo url('assets/images/favicon.png') ?>">
 
     <link href='https://fonts.googleapis.com/css?family=Fira+Sans:400,400italic,700,700italic&subset=greek,latin' rel='stylesheet' type='text/css'>
